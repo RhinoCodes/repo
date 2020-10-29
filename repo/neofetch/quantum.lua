905f27934131ecf946e0c805908b598ab28d7b95
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2020-10-29 17:21:29 -0500

    Update quantum.lua

--- repo/neofetch/quantum.lua
+++ repo/neofetch/quantum.lua
@@ -6,5 +6,5 @@ package = {
 }
 
 function install()
-  quantum_install("neofetch", "20489a51e494a9a3d91eb41b14175137bd175480acda8ad5d34965d46a64cfa711a1664ff0cf5975984dfd0e186a6a57da9bceaedd860880e0dd3a0bbfbbeb9c")
+  quantum_install("neofetch", "b1f868911a9f59f6e6a3f7f4e62b484869cfbb9ac8e345bb54d3bfa4988fb74fff7fde46d6a201494861ce193e7ee6f294485f90221ea27ff6851e80f18ff2be")
 end
