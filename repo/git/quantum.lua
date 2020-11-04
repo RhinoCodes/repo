b17af8797b1ffaa722fc44848e99835468bbfcb8
Will Forsberg <forsbergw82@gmail.com>
2020-11-04 17:24:44 -0600

    add git (;

---
+++ repo/git/quantum.lua
@@ -0,0 +1,10 @@
+package = {
+  name = "git",
+  version = "2.9.5",
+  source = "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.9.5.tar.xz",
+  git = false
+}
+
+function install()
+  quantum_install("pfetch", "a4bea37ecb9a3fb5c0c8ef18c2f7eeaf8ccbcfec91f72f3bccfc6bf72a3e3902")
+end
