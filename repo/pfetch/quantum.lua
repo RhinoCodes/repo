9eec03e418bde24070dec582b7091b4720a55b0d
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2020-11-04 17:13:13 -0600

    Update quantum.lua

--- repo/pfetch/quantum.lua
+++ repo/pfetch/quantum.lua
@@ -6,5 +6,5 @@ package = {
 }
 
 function install()
-  quantum_install("pfetch", "ca26517d70b63f2766386108e3e491d69d3ab21d49fde3fc7da2b57b329a19ad87df7ff058b9117ed3f75a8fe6f36e48d55e456d31afc7a8b54ad2c7b635a707")
+  quantum_install("*", "ca26517d70b63f2766386108e3e491d69d3ab21d49fde3fc7da2b57b329a19ad87df7ff058b9117ed3f75a8fe6f36e48d55e456d31afc7a8b54ad2c7b635a707")
 end
