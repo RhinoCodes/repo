f808e57acd0004a9c5caf233273d471ab464de45
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2020-11-06 17:57:04 -0600

    Update quantum.lua

--- repo/git/quantum.lua
+++ repo/git/quantum.lua
@@ -1,4 +1,4 @@
-package = {
+  package = {
   name = "git",
   version = "2.9.5",
   source = "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.9.5.tar.xz",
@@ -11,5 +11,6 @@ end
 
 function install()
   os.execute("mkdir -p ~/quantum-lua/bindir/git/2.9.5/bin  ~/quantum-lua/bindir/git/2.9.5/lib64  ~/quantum-lua/bindir/git/2.9.5/libexec  ~/quantum-lua/bindir/git/2.9.5/share")
-  os.execute("make install prefix=~/quantum-lua/bindir/git/2.9.5", 1)
+  os.execute("make install prefix=$HOME/quantum-lua/bindir/git/2.9.5")
+  quantum_install("bin/git")
 end
