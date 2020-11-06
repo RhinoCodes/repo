a15cc49d775d0f3ea1e96d2e680643febabaddd3
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2020-11-06 17:14:23 -0600

    Update quantum.lua

--- repo/git/quantum.lua
+++ repo/git/quantum.lua
@@ -10,5 +10,6 @@ function build()
 end
 
 function install()
+  os.execute("mkdir -p ~/quantum-lua/bindir/git/2.9.5/bin  ~/quantum-lua/bindir/git/2.9.5/lib64  ~/quantum-lua/bindir/git/2.9.5/libexec  ~/quantum-lua/bindir/git/2.9.5/share")
   os.execute("make install prefix=~/quantum-lua/bindir/git/2.9.5", 1)
 end
