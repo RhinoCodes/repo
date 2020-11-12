23290b4185ceb2c8b02b7f31f315827fdd0593b4
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2020-11-12 16:19:47 -0600

    Update quantum.lua

--- repo/pfetch/quantum.lua
+++ repo/pfetch/quantum.lua
@@ -2,9 +2,11 @@ package = {
   name = "pfetch",
   version = "0.6.0",
   source = "https://github.com/dylanaraps/pfetch/archive/0.6.0.tar.gz",
-  git = false
+  git = false,
+  "neofetch"
 }
 
 function install()
   quantum_install("pfetch", false)
 end
+-- I use pfetch as a testing ground - so if it doesn't install, you know why!
