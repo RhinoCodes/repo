21aeec985ac96a1241cf3df79ea3813095417a23
Will Forsberg <forsbergw82@gmail.com>
2020-12-18 15:54:55 -0600

    add package for kernel

---
+++ repo/linux5.4/quantum.lua
@@ -0,0 +1,17 @@
+package = {
+    name = "linux",
+    version = "5.4.77",
+    source = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.77.tar.xz",
+    git = false
+}
+
+function build()
+    os.system("make x86_64-defconfig")
+    make()
+end
+
+function install()
+    os.system("make INSTALL_MOD_PATH=" .. install_dir .. "/bindir/linux/5.4.77" .. " modules_install" )
+end
+
+function install
