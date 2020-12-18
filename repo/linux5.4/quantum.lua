c04e73f300303bdb1cf336c4cb16c06af7fba309
Will Forsberg <forsbergw82@gmail.com>
2020-12-18 17:34:31 -0600

    update linux buildfile

--- repo/linux5.4/quantum.lua
+++ repo/linux5.4/quantum.lua
@@ -2,7 +2,8 @@ package = {
     name = "linux",
     version= "5.4.77",
     source = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.77.tar.xz",
-    git = false
+    git = false,
+    root = true
 }
 
 function build()
@@ -12,4 +13,9 @@ end
 
 function install()
     os.execute("make INSTALL_MOD_PATH=" .. install_dir .. "/bindir/linux/5.4.77" .. " modules_install" )
+    os.execute("cp -v arch/x86/boot/bzImage " .. install_dir .. "/boot/vmlinuz-5.4.77")
+    os.execute("cp -v System.map " .. install_dir .. "/boot/System.map-5.4.77")
+    os.execute("cp -v .config " .. install_dir .. "/boot/config-5.4.77")
+    os.execute("ln -s " .. install_dir .. "/boot/* /boot/")
+    os.execute("ln -s " .. install_dir .. "/lib/modules/5.4.77 /usr/lib/modules/5.4.77")
 end 
