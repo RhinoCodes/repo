4194d68275477fcc8ba2b5348cf06f6f164bf7b8
Will Forsberg <forsbergw82@gmail.com>
2020-12-22 15:57:39 -0600

    add busybox

---
+++ repo/busybox/quantum.lua
@@ -0,0 +1,28 @@
+package = {
+    name = "busybox",
+    version = "1.32.0",
+    source = "https://busybox.net/downloads/busybox-1.32.0.tar.bz2",
+    git = false,
+    root = true
+}
+
+function build()
+    os.execute("make defconfig")
+    make()
+end
+
+function install()
+    os.execute("make CONFIG_PREFIX=" .. install_dir .. " install")
+    os.execute("mkdir -p " .. root .. "/{bin,sbin,usr,usr/bin,usr/sbin}")
+    os.execute("ln -s " .. install_dir .. "/bin/* " .. root .. "/bin")
+    os.execute("ln -s " .. install_dir .. "/sbin/* " .. root .. "/sbin")
+    os.execute("ln -s " .. install_dir .. "/usr/sbin/* " .. root .. "/usr/sbin")
+    os.execute("ln -s " .. install_dir .. "/usr/bin/* " .. root .. "/usr/bin")
+    os.execute("ln -s " .. install_dir .. "/linuxrc " .. root .. "/linuxrc")
+end
+
+function uninstall()
+    os.execute("unlink " .. root .. "/linuxrc")
+    os.execute("rm -rf " .. install_dir)
+    os.execute("find " .. root .. "{/bin,/sbin,/usr/bin,/usr/sbin} -xtype l -delete")
+end
